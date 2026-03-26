109376678a357ebb2f3d40f2e19ce39e2ef3169d
Ross Cawston <rcawston@users.noreply.github.com>
2026-03-26 16:17:43 -0700

    Update AppImage builder to use system Python 3 on Ubuntu 24.04

--- util/linux-builder/_builder.sh
+++ util/linux-builder/_builder.sh
@@ -2,13 +2,12 @@
 
 set -e
 
-export LD_LIBRARY_PATH=/vial-gui/util/python36/prefix/lib/
-
 cd /vial-gui
 python3 -m venv docker_venv
 . docker_venv/bin/activate
+python -m pip install --upgrade pip
 pip install -r requirements.txt
-pyinstaller vial.spec
+pyinstaller --noconfirm vial.spec
 deactivate
 /pkg2appimage-*/pkg2appimage misc/Vial.yml
 mv out/Vial-*.AppImage /output/Vial-x86_64.AppImage
